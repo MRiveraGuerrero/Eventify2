1ab00ccd58425101adab936fc1463999e750535c
AnderG2003 <agorocica002@ikasle.ehu.eus>
2024-11-07 17:45:34 +0100

    He agregado lo de los likes

--- database.sql
+++ database.sql
@@ -56,17 +56,18 @@ CREATE TABLE `eventos` (
   `opcion1` varchar(255) NOT NULL,
   `resultado1` varchar(255) NOT NULL,
   `opcion2` varchar(255) NOT NULL,
-  `resultado2` varchar(255) NOT NULL
+  `resultado2` varchar(255) NOT NULL,
+  `likes` int(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `eventos`
 --
 
-INSERT INTO `eventos` (`usuario`, `titulo`, `enunciado`, `opcion1`, `resultado1`, `opcion2`, `resultado2`) VALUES
-('Imanol', 'Las aventuras de WIP games', 'Era un día soleado cuando nos encontramos una cueva misteriosa, estaba muy oscura y nos dieron ganas de entrar. Poco a poco se hacía la luz dentro de la cueva y de repente vimos una estatua gigante rodeada de oro', 'Oro! Tenemos que coger todo lo que podamos! Nos haremos ricos!!', 'Era una trampa, hemos caido en un agujero sin salida...', 'No hacemos nada, podría ser una trampa', 'Nos vamos con las manos vacías pero con una increible historia que contar a nuestros hijos'),
-('ImanolMM', '¡¡Enanos!!', 'Te despiertas de una larga siesta y estas rodeado de enanos, Quita! Son demasiados y te estan intentando agarrar para meterte en una caja! Despues de un tiempo siendo transportado ves que te han llevado a su aldea.', 'Te intentas liberar y peleas contra ellos', 'Te dañan pero consigues escapar', 'Usas tu linterna para intentar sorprenderles', 'Están sorprendidos. Nunca antes habían visto algo así,  te toman por su dios y te dan de comer y beber'),
-('invitado', 'Cueva misteriosa', 'Mientras dabas un paseo para pasar el tiempo te encuentras con la entrada de una cueva y como no tenias nada mejor que hacer te adentras en ella. Ves una puerta metálica', 'intentas entrar (podría ser peligroso)', 'La puerta está tan dura que nos consigues abrirla', 'Esperas un rato a ver que pasa', 'Tras esperar un par de horas se habre la puerta donde sale un hombre vestido de negro, sin que se diera cuenta te escabulles denro de la sala. Dentro consigues reunir materiales y escapas de la isla.');
+INSERT INTO `eventos` (`usuario`, `titulo`, `enunciado`, `opcion1`, `resultado1`, `opcion2`, `resultado2`, `likes`) VALUES
+('Imanol', 'Las aventuras de WIP games', 'Era un día soleado cuando nos encontramos una cueva misteriosa, estaba muy oscura y nos dieron ganas de entrar. Poco a poco se hacía la luz dentro de la cueva y de repente vimos una estatua gigante rodeada de oro', 'Oro! Tenemos que coger todo lo que podamos! Nos haremos ricos!!', 'Era una trampa, hemos caido en un agujero sin salida...', 'No hacemos nada, podría ser una trampa', 'Nos vamos con las manos vacías pero con una increible historia que contar a nuestros hijos', 0),
+('ImanolMM', '¡¡Enanos!!', 'Te despiertas de una larga siesta y estas rodeado de enanos, Quita! Son demasiados y te estan intentando agarrar para meterte en una caja! Despues de un tiempo siendo transportado ves que te han llevado a su aldea.', 'Te intentas liberar y peleas contra ellos', 'Te dañan pero consigues escapar', 'Usas tu linterna para intentar sorprenderles', 'Están sorprendidos. Nunca antes habían visto algo así,  te toman por su dios y te dan de comer y beber', 0),
+('invitado', 'Cueva misteriosa', 'Mientras dabas un paseo para pasar el tiempo te encuentras con la entrada de una cueva y como no tenias nada mejor que hacer te adentras en ella. Ves una puerta metálica', 'intentas entrar (podría ser peligroso)', 'La puerta está tan dura que nos consigues abrirla', 'Esperas un rato a ver que pasa', 'Tras esperar un par de horas se habre la puerta donde sale un hombre vestido de negro, sin que se diera cuenta te escabulles denro de la sala. Dentro consigues reunir materiales y escapas de la isla.', 0);
 
 -- --------------------------------------------------------
 
@@ -82,7 +83,8 @@ CREATE TABLE `usuarios` (
   `nacimiento` date NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `passwd` varchar(255) NOT NULL,
-  `sal` varchar(50) NOT NULL
+  `sal` varchar(50) NOT NULL,
+  `cookie` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -125,6 +127,15 @@ ALTER TABLE `usuarios`
 --
 -- Restricciones para tablas volcadas
 --
+
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes` (
+  `usuarioCreador` varchar(50) NOT NULL,
+  `tituloEv` varchar(255) NOT NULL,
+  `usuarioLike` varchar(50) NOT NULL,
+  PRIMARY KEY (`usuarioCreador`, `tituloEv`, `usuarioLike`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 --
 -- Filtros para la tabla `accesos`
